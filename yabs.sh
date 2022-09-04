e211340fa09ec30124b5089ce6d1e93334ece19c
Gary Morris <16860672+fattox@users.noreply.github.com>
2022-09-04 09:03:25 +0200

    Update yabs.sh

--- yabs.sh
+++ yabs.sh
@@ -352,12 +352,12 @@ function disk_test {
 	if [[ "$ARCH" = "aarch64" || "$ARCH" = "arm" ]]; then
 		FIO_SIZE=512M
 	else
-		FIO_SIZE=2G
+		FIO_SIZE=20G
 	fi
 
 	# run a quick test to generate the fio test file to be used by the actual tests
 	echo -en "Generating fio test file..."
-	$FIO_CMD --name=setup --ioengine=libaio --rw=read --bs=64k --iodepth=64 --numjobs=2 --size=$FIO_SIZE --runtime=1 --gtod_reduce=1 --filename=$DISK_PATH/test.fio --direct=1 --minimal &> /dev/null
+	$FIO_CMD --name=setup --ioengine=libaio --rw=read --bs=64k --iodepth=64 --numjobs=2 --size=$FIO_SIZE --runtime=60 --ramp_time=5 --gtod_reduce=1 --direct=1 --filename=$DISK_PATH/test.fio --minimal &> /dev/null
 	echo -en "\r\033[0K"
 
 	# get array of block sizes to evaluate
@@ -366,7 +366,7 @@ function disk_test {
 	for BS in "${BLOCK_SIZES[@]}"; do
 		# run rand read/write mixed fio test with block size = $BS
 		echo -en "Running fio random mixed R+W disk test with $BS block size..."
-		DISK_TEST=$(timeout 35 $FIO_CMD --name=rand_rw_$BS --ioengine=libaio --rw=randrw --rwmixread=50 --bs=$BS --iodepth=64 --numjobs=2 --size=$FIO_SIZE --runtime=60 --ramp_time=5 --gtod_reduce=1 --direct=1 --filename=$DISK_PATH/test.fio --group_reporting --minimal 2> /dev/null | grep rand_rw_$BS)
+		DISK_TEST=$(timeout 35 $FIO_CMD --ioengine=libaio --rw=randrw --bs=$BS --iodepth=64 --numjobs=2 --size=$FIO_SIZE --runtime=60 --ramp_time=5 --gtod_reduce=1 --direct=1 --filename=$DISK_PATH/test.fio --minimal --name=rand_rw_$BS --rwmixread=50 --group_reporting 2> /dev/null | grep rand_rw_$BS)
 		DISK_IOPS_R=$(echo $DISK_TEST | awk -F';' '{print $8}')
 		DISK_IOPS_W=$(echo $DISK_TEST | awk -F';' '{print $49}')
 		DISK_IOPS=$(awk -v a="$DISK_IOPS_R" -v b="$DISK_IOPS_W" 'BEGIN { print a + b }')
